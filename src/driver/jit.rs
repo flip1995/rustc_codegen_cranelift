5853c71dcef138e8213ba7563a32ff583615dc2b
bjorn3 <bjorn3@users.noreply.github.com>
2020-10-11 13:34:44 +0200

    [WIP]

--- src/driver/jit.rs
+++ src/driver/jit.rs
@@ -1,6 +1,8 @@
 //! The JIT driver uses [`cranelift_simplejit`] to JIT execute programs without writing any object
 //! files.
 
+use std::any::Any;
+use std::cell::RefCell;
 use std::ffi::CString;
 use std::os::raw::{c_char, c_int};
 
@@ -9,7 +11,31 @@ use rustc_middle::mir::mono::MonoItem;
 
 use cranelift_simplejit::{SimpleJITBuilder, SimpleJITModule};
 
-use crate::prelude::*;
+use crate::{prelude::*, CodegenCx};
+
+thread_local! {
+    pub static EXISTING_SYMBOLS: RefCell<FxHashMap<String, *const u8>> = RefCell::new(FxHashMap::default());
+    pub static INSTANCE_CODEGEN: RefCell<FxHashMap<Instance<'static>, *const u8>> = RefCell::new(FxHashMap::default());
+}
+
+fn make_jit(tcx: TyCtxt<'_>) -> SimpleJITModule {
+    let existing_symbols: Vec<_> = EXISTING_SYMBOLS.with(|existing_symbols| {
+        existing_symbols
+            .borrow()
+            .iter()
+            .map(|(name, &addr)| (name.clone(), addr))
+            .collect()
+    });
+
+    let mut jit_builder = SimpleJITBuilder::with_isa(
+        crate::build_isa(tcx.sess, false),
+        cranelift_module::default_libcall_names(),
+    );
+    jit_builder.symbols(existing_symbols);
+    let jit_module = SimpleJITModule::new(jit_builder);
+    assert_eq!(pointer_ty(tcx), jit_module.target_config().pointer_type());
+    jit_module
+}
 
 pub(super) fn run_jit(tcx: TyCtxt<'_>) -> ! {
     if !tcx.sess.opts.output_types.should_codegen() {
@@ -34,15 +60,12 @@ pub(super) fn run_jit(tcx: TyCtxt<'_>) -> ! {
         );
     }
 
-    let imported_symbols = load_imported_symbols_for_jit(tcx);
+    EXISTING_SYMBOLS.with(|existing_symbols| {
+        assert!(existing_symbols.borrow().is_empty());
+        *existing_symbols.borrow_mut() = load_imported_symbols_for_jit(tcx);
+    });
 
-    let mut jit_builder = SimpleJITBuilder::with_isa(
-        crate::build_isa(tcx.sess, false),
-        cranelift_module::default_libcall_names(),
-    );
-    jit_builder.symbols(imported_symbols);
-    let mut jit_module = SimpleJITModule::new(jit_builder);
-    assert_eq!(pointer_ty(tcx), jit_module.target_config().pointer_type());
+    let mut jit_module = make_jit(tcx);
 
     let sig = Signature {
         params: vec![
@@ -72,16 +95,18 @@ pub(super) fn run_jit(tcx: TyCtxt<'_>) -> ! {
     let (mut jit_module, global_asm, _debug, mut unwind_context) =
         super::time(tcx, "codegen mono items", || {
             super::predefine_mono_items(&mut cx, &mono_items);
-            for (mono_item, (linkage, visibility)) in mono_items {
-                let linkage = crate::linkage::get_clif_linkage(mono_item, linkage, visibility);
+            for &(mono_item, _) in &mono_items {
+                let tcx = cx.tcx;
                 match mono_item {
                     MonoItem::Fn(inst) => {
-                        cx.tcx.sess.time("codegen fn", || {
-                            crate::base::trans_fn(&mut cx, inst, linkage)
+                        let _inst_guard = crate::PrintOnPanic(|| {
+                            format!("{:?} {}", inst, tcx.symbol_name(inst).name)
                         });
+                        debug_assert!(!inst.substs.needs_infer());
+                        codegen_shim(&mut cx, inst);
                     }
                     MonoItem::Static(def_id) => {
-                        crate::constant::codegen_static(&mut cx.constants_cx, def_id)
+                        crate::constant::codegen_static(&mut cx.constants_cx, def_id);
                     }
                     MonoItem::GlobalAsm(hir_id) => {
                         let item = cx.tcx.hir().expect_item(hir_id);
@@ -90,6 +115,31 @@ pub(super) fn run_jit(tcx: TyCtxt<'_>) -> ! {
                     }
                 }
             }
+
+            cx.module.finalize_definitions();
+            EXISTING_SYMBOLS.with(|existing_symbols| {
+                for &(mono_item, _) in &mono_items {
+                    let inst = match mono_item {
+                        MonoItem::Fn(inst) => inst,
+                        MonoItem::Static(_) | MonoItem::GlobalAsm(_) => continue,
+                    };
+                    let (name, sig) = crate::abi::get_function_name_and_sig(
+                        cx.tcx,
+                        cx.module.isa().triple(),
+                        inst,
+                        true,
+                    );
+                    let func_id = cx
+                        .module
+                        .declare_function(&name, Linkage::Export, &sig)
+                        .unwrap();
+                    let ptr = cx.module.get_finalized_function(func_id);
+                    existing_symbols
+                        .borrow_mut()
+                        .insert(name, *Any::downcast_ref::<*const u8>(&ptr).unwrap());
+                }
+            });
+
             tcx.sess.time("finalize CodegenCx", || cx.finalize())
         });
     if !global_asm.is_empty() {
@@ -127,7 +177,60 @@ pub(super) fn run_jit(tcx: TyCtxt<'_>) -> ! {
     std::process::exit(ret);
 }
 
-fn load_imported_symbols_for_jit(tcx: TyCtxt<'_>) -> Vec<(String, *const u8)> {
+#[no_mangle]
+pub extern "C" fn __clif_jit_fn(instance_ptr: *const Instance<'static>) -> *const u8 {
+    if let Some(f) = INSTANCE_CODEGEN.with(|instance_codegen| {
+        if let Some(&f) = instance_codegen.borrow().get(unsafe { &*instance_ptr }) {
+            println!("Existing @ {:p}", f);
+            Some(f)
+        } else {
+            None
+        }
+    }) {
+        return f;
+    }
+
+    rustc_middle::ty::tls::with(|tcx| {
+        // lift is used to ensure the correct lifetime for instance.
+        let instance = tcx.lift(unsafe { &*instance_ptr }).unwrap();
+
+        let jit_module = make_jit(tcx);
+        let mut cx = crate::CodegenCx::new(tcx, jit_module, false);
+
+        tcx.sess.time("codegen fn", || {
+            crate::base::trans_fn(&mut cx, instance, Linkage::Export)
+        });
+
+        let (name, sig) =
+            crate::abi::get_function_name_and_sig(tcx, cx.module.isa().triple(), instance, true);
+        let func_id = cx
+            .module
+            .declare_function(&name, Linkage::Export, &sig)
+            .unwrap();
+
+        let (mut jit_module, global_asm, _debug_context, _unwind_context) = cx.finalize();
+        assert!(global_asm.is_empty());
+        jit_module.finalize_definitions();
+        let ptr = jit_module.get_finalized_function(func_id);
+        jit_module.finish();
+
+        INSTANCE_CODEGEN.with(|instance_codegen| {
+            instance_codegen
+                .borrow_mut()
+                .insert(unsafe { (&*instance_ptr).clone() }, ptr);
+        });
+
+        EXISTING_SYMBOLS.with(|existing_symbols| {
+            existing_symbols.borrow_mut().insert(name, ptr);
+        });
+
+        println!("New {:?} @ {:p}", instance, ptr);
+
+        ptr
+    })
+}
+
+fn load_imported_symbols_for_jit(tcx: TyCtxt<'_>) -> FxHashMap<String, *const u8> {
     use rustc_middle::middle::dependency_format::Linkage;
 
     let mut dylib_paths = Vec::new();
@@ -157,7 +260,7 @@ fn load_imported_symbols_for_jit(tcx: TyCtxt<'_>) -> Vec<(String, *const u8)> {
         }
     }
 
-    let mut imported_symbols = Vec::new();
+    let mut imported_symbols = FxHashMap::default();
     for path in dylib_paths {
         use object::Object;
         let lib = libloading::Library::new(&path).unwrap();
@@ -185,4 +288,69 @@ fn load_imported_symbols_for_jit(tcx: TyCtxt<'_>) -> Vec<(String, *const u8)> {
     tcx.sess.abort_if_errors();
 
     imported_symbols
+}
+
+pub(super) fn codegen_shim<'tcx>(cx: &mut CodegenCx<'tcx, impl Module>, inst: Instance<'tcx>) {
+    let tcx = cx.tcx;
+
+    let pointer_type = cx.module.target_config().pointer_type();
+
+    let (name, sig) =
+        crate::abi::get_function_name_and_sig(tcx, cx.module.isa().triple(), inst, true);
+    let func_id = cx
+        .module
+        .declare_function(&name, Linkage::Export, &sig)
+        .unwrap();
+
+    let instance_ptr = Box::into_raw(Box::new(inst));
+
+    let jit_fn = cx
+        .module
+        .declare_function(
+            "__clif_jit_fn",
+            Linkage::Import,
+            &Signature {
+                call_conv: cx.module.target_config().default_call_conv,
+                params: vec![AbiParam::new(pointer_type)],
+                returns: vec![AbiParam::new(pointer_type)],
+            },
+        )
+        .unwrap();
+
+    let mut trampoline = Function::with_name_signature(ExternalName::default(), sig.clone());
+    let mut builder_ctx = FunctionBuilderContext::new();
+    let mut trampoline_builder = FunctionBuilder::new(&mut trampoline, &mut builder_ctx);
+
+    let jit_fn = cx
+        .module
+        .declare_func_in_func(jit_fn, trampoline_builder.func);
+    let sig_ref = trampoline_builder.func.import_signature(sig);
+
+    let entry_block = trampoline_builder.create_block();
+    trampoline_builder.append_block_params_for_function_params(entry_block);
+    let fn_args = trampoline_builder
+        .func
+        .dfg
+        .block_params(entry_block)
+        .to_vec();
+
+    trampoline_builder.switch_to_block(entry_block);
+    let instance_ptr = trampoline_builder
+        .ins()
+        .iconst(pointer_type, instance_ptr as u64 as i64);
+    let jitted_fn = trampoline_builder.ins().call(jit_fn, &[instance_ptr]);
+    let jitted_fn = trampoline_builder.func.dfg.inst_results(jitted_fn)[0];
+    let call_inst = trampoline_builder
+        .ins()
+        .call_indirect(sig_ref, jitted_fn, &fn_args);
+    let ret_vals = trampoline_builder.func.dfg.inst_results(call_inst).to_vec();
+    trampoline_builder.ins().return_(&ret_vals);
+
+    cx.module
+        .define_function(
+            func_id,
+            &mut Context::for_function(trampoline),
+            &mut cranelift_codegen::binemit::NullTrapSink {},
+        )
+        .unwrap();
 }
